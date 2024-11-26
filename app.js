ae47876e0a16a311df23aaa387f6602cc3fc9b9d
tbartsctr <tbartsctr@gmail.com>
2024-11-25 23:56:37 -0500

    ALAB 308 adds

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-let n = 463;
-function isPrime(num) { if (num <= 1) return false;
-    for (let i = 2; i < num; i++) {
-    if (num % i === 0) return false;}
-    return true;
+const PI = 3.1315;
+const radius = 5;
+const area = PI * radius * radius;
+const plantSpace = 0.8;
+const maxPlants = area / plantSpace;
 
-}
+let plants = 20;
 
-while (true) {
-    if (isPrime(n)){console.log(n);
-        break;
-}
-n++;
+function predictGrowth(weeks) {plants * Math.pow(2, weeks);
+    const plantPercentage = (plants / maxPlants) * 100;
+    if (plantPercentage > 80) {return "Prune Plants";}
+    else if (plantPercentage <= 80 && plantPercentage >= 50); {return "Monitor";}
+    else {return "Plant More";}
+
+
+
 }
