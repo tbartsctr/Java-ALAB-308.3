98cb8f09ed03968a7c30e5042ebd01795b400d99
tbartsctr <tbartsctr@gmail.com>
2024-11-26 00:14:23 -0500

    ALAB308.1 add

--- app.js
+++ app.js
@@ -4,14 +4,15 @@ const area = PI * radius * radius;
 const plantSpace = 0.8;
 const maxPlants = area / plantSpace;
 
-let plants = 20;
+let plants = 100;
 
-function predictGrowth(weeks) {plants * Math.pow(2, weeks);
-    const plantPercentage = (plants / maxPlants) * 100;
-    if (plantPercentage > 80) {return "Prune Plants";}
-    else if (plantPercentage <= 80 && plantPercentage >= 50); {return "Monitor";}
-    else {return "Plant More";}
-
-
-
+function predictGrowth(weeks, plants) {
+    let plantCount = plants * Math.pow(2, weeks);
+    const requiredArea = plantCount * plantSpace;
+    const radius = Math.sqrt(requiredArea / PI);
+    return { plantCount, requiredArea, radius};
 }
+
+
+
+
